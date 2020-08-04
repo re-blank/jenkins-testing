cfc264ae78d96ba84862e08bc3d227d7098e5abd
Mehrab <Mehrab.Rahman@Revature.com>
2020-08-04 11:45:41 -0500

    server prototype

---
+++ src/main/java/com/github/MehrabRahman/Server.java
@@ -0,0 +1,31 @@
+package com.github.MehrabRahman;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(8080);
+            Socket socket = server.accept();
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(inputStream);
+            BufferedReader reader = new BufferedReader(isr);
+            while (reader.ready()) {
+                System.out.println(reader.readLine());
+            }
+
+            OutputStream outputStream = socket.getOutputStream();
+            PrintStream printStream = new PrintStream(outputStream);
+            printStream.println("Hello");
+        } catch (IOException e) {
+            System.err.println("Failed to create server");
+        }
+    }
+}
