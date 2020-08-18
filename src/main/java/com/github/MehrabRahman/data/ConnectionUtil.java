456ab647d9aece2c92997dc082e13f7f6404556d
Mehrab <Mehrab.Rahman@Revature.com>
2020-08-17 20:57:07 -0500

    Fixing bad jdbc url

--- src/main/java/com/github/MehrabRahman/data/ConnectionUtil.java
+++ src/main/java/com/github/MehrabRahman/data/ConnectionUtil.java
@@ -8,7 +8,7 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection("localhost:5432/httpdb", "httpdb", "httpdb");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/httpdb", "httpdb", "httpdb");
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
